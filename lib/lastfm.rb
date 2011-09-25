7595a7c329f77fa96af310e92b2b03e7a03fe0cf
Jarrett Hawrylak <jjhawrylak+git@gmail.com>
2011-09-24 21:42:28 -0400

    did something..

--- lib/lastfm.rb
+++ lib/lastfm.rb
@@ -2,9 +2,24 @@ require 'net/http'
 require 'uri'
 
 class LastFM
-  
+  @@api_base = "http://ws.audioscrobbler.com/2.0/?"
   def initialize(apikey,secret)
     @apikey = apikey
     @secret = secret
+  end
+  
+  def domethod(method,*hash)
+    args = "method=#{method}"
+    unless hash[0].nil?
+      if hash[0].instance_of? Hash
+        hash[0].each { |k,v| args += "&#{k.to_s}=#{v}"}
+      elsif 
+        args += "&"
+        args += hash.join('&')
+      end
+    end
+    args += "&api_key=#{@apikey}"
+    url = @@api_base + args
+    puts url
   end
 end
